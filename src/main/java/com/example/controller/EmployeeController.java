4e6c5ce18847152b5937b693fa73579ba4aa7274
srinivas350 <srinivasarao.s350@gmail.com>
2023-09-27 22:09:31 +0530

    Added Security

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,15 +16,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.entity.AuthRequest;
 import com.example.entity.Employee;
 import com.example.entity.InputEmployee;
+import com.example.jwt.api.util.JwtUtil;
 import com.example.service.EmployeeService;
 
 @RestController
-@RequestMapping("/employee")
 public class EmployeeController {
 	@Autowired
 	public EmployeeService service;
+	
+	@Autowired
+    private JwtUtil jwtUtil;
+	
+    @Autowired
+    private AuthenticationManager authenticationManager;
 	
 	@PostMapping("/insert")
 	public ResponseEntity<InputEmployee> insertEmployee(@RequestBody InputEmployee employee)
@@ -49,5 +58,17 @@ public class EmployeeController {
 	{
 		return "Hello "+msg;
 	}
+	
+	@PostMapping("/authenticate")
+    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword())
+            );
+        } catch (Exception ex) {
+            throw new Exception("inavalid username/password");
+        }
+        return jwtUtil.generateToken(authRequest.getUserName());
+    }
 		
 }
--- src/main/java/com/example/demo/SpringBootH2ExampleApplication.java
+++ src/main/java/com/example/demo/SpringBootH2ExampleApplication.java
@@ -1,16 +1,39 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.example.entity.User;
+import com.example.repositories.UserRepository;
+
 @SpringBootApplication
 @ComponentScan("com.example")
 @EnableJpaRepositories("com.example")
 @EntityScan("com.example")
 public class SpringBootH2ExampleApplication {
+	
+	@Autowired
+	public UserRepository repository;
+	@PostConstruct
+    public void initUsers() {
+        List<User> users = Stream.of(
+                new User(101, "javatechie", "password", "javatechie@gmail.com"),
+                new User(102, "user1", "pwd1", "user1@gmail.com"),
+                new User(103, "user2", "pwd2", "user2@gmail.com"),
+                new User(104, "user3", "pwd3", "user3@gmail.com")
+        ).collect(Collectors.toList());
+        repository.saveAll(users);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootH2ExampleApplication.class, args);
